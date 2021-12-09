33f0e30f6d937fb9803edabe88ec7a42e70eeb51
morihirok <omnimani3@gmail.com>
2021-12-09 23:56:19 +0900

    p86

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -276,3 +276,23 @@ func TestParsingInfixExpressions(t *testing.T) {
 		}
 	}
 }
+
+func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
+	ident, ok := exp.(*ast.Identifier)
+	if !ok {
+		t.Errorf("exp not *ast.Identifier. got=%T", exp)
+		return false
+	}
+
+	if ident.Value != value {
+		t.Errorf("ident.Value not %s. got=%T", value, ident.Value)
+		return false
+	}
+
+	if ident.TokenLiteral() != value {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", value, ident.TokenLiteral())
+		return false
+	}
+
+	return true
+}
